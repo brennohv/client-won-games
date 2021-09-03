92c7222e4c8041f1da4cf27e52b23f4ad9038be3
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-09-03 16:12:33 +0100

    create style logo component, color, height and width

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,3 +1,22 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main``
+import { LogoProps } from '.'
+
+const wrapperModifiers = {
+  normal: () => css`
+    width: 11rem;
+    height: 3.3rem;
+  `,
+  large: () => css`
+    width: 20rem;
+    height: 5.9rem;
+  `
+}
+
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, color, size }) => css`
+    color: ${theme.colors[color!]};
+
+    ${!!size && wrapperModifiers[size]}
+  `}
+`

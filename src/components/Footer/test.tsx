d4e2c8c57b07740b299b1fa6f9839bfb7e30a1c6
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-09-16 11:18:54 +0100

    Create Footer Component and tests

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Footer from '.'
+
+describe('<Footer />', () => {
+  it('should render 4 column topics', () => {
+    renderWithTheme(<Footer />)
+
+    expect(
+      screen.getByRole('heading', { name: /Contact Us/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Follow us/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Links/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Location/i })
+    ).toBeInTheDocument()
+  })
+})

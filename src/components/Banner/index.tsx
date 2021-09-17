ad41ccafefc6256a91c3928daa56879093248a17
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-09-17 14:48:31 +0100

    Create Banner Component with styles and tests

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,39 @@
+import Button from 'components/Button'
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
+
+export type BannerProps = {
+  img: string
+  title?: string
+  subtitle?: string
+  buttonLabel?: string
+  buttonLink?: string
+}
+
+const Banner = ({
+  img,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink
+}: BannerProps) => (
+  <S.Wrapper>
+    <S.Image src={img} role="img" aria-label={title}></S.Image>
+    <S.Caption>
+      <S.Title>{title}</S.Title>
+      <S.SubTitle dangerouslySetInnerHTML={{ __html: subtitle! }} />
+      <MediaMatch lessThan="medium">
+        <Button size={'medium'} as="a" href={buttonLink}>
+          {buttonLabel}
+        </Button>
+      </MediaMatch>
+      <MediaMatch greaterThan="medium">
+        <Button size={'large'} as="a" href={buttonLink}>
+          {buttonLabel}
+        </Button>
+      </MediaMatch>
+    </S.Caption>
+  </S.Wrapper>
+)
+
+export default Banner
---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,76 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.div`
+  position: relative;
+
+  ${media.greaterThan('medium')`
+  box-shadow: 0 0.4rem 0.5rem 0 rgba(0, 0, 0, 0.2);
+  `}
+`
+
+export type ImageProps = {
+  src?: string
+}
+
+export const Image = styled.div<ImageProps>`
+  ${({ theme, src }) => css`
+    width: 100%;
+    height: 23rem;
+    background: ${theme.colors.lightGray};
+    background-image: url(${src});
+    background-position: center center;
+    background-size: cover;
+
+    ${media.greaterThan('medium')`
+      height: 58rem
+    `}
+  `}
+`
+
+export const Caption = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    /* height: 14rem; */
+    padding: ${theme.spacings.small};
+
+    ${media.greaterThan('medium')`
+    height: 22rem;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    border-radius: ${theme.border.radius} ;
+    padding: ${theme.spacings.medium} ${theme.spacings.large};
+    `}
+  `}
+`
+
+export const Title = styled.h2`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge}
+    `}
+  `}
+`
+
+export const SubTitle = styled.h3`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.normal};
+    margin-bottom: ${theme.spacings.xxsmall};
+
+    ${media.greaterThan('medium')`
+      margin-bottom: ${theme.spacings.xsmall};
+      font-size: ${theme.font.sizes.xlarge}
+    `}
+
+    strong {
+      color: ${theme.colors.primary};
+    }
+  `}
+`

d66241873899daf0109bba260b2159f016482e15
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-10-02 15:12:29 +0100

    Create Container Component

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,29 @@
+// import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Container } from '.'
+
+describe('<Container />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(<Container />)
+
+    expect(container.firstChild).toHaveStyle({
+      maxWidth: theme.grid.container
+    })
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+.c0 {
+  max-width: 130rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-right: calc(3.2rem / 2);
+  padding-left: calc(3.2rem / 2);
+}
+
+<div
+  class="c0"
+/>
+`)
+  })
+})

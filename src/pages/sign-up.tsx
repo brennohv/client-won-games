df4c451e0a0ac88ae9ff9ae72f8c3898d36fe9f5
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-10-17 17:45:27 +0100

    Add Forms to Sign In and Sign Up pages

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
 import Auth from 'templates/Auth'
 
 export default function SignUp() {
-  return <Auth title="SingUp" />
+  return (
+    <Auth title="Sing Up">
+      <FormSignUp />
+    </Auth>
+  )
 }

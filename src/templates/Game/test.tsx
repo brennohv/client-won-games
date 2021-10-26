c452334c581a393a21c65fe4e22231b854f8df1e
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-10-26 09:44:14 +0100

    Add getStaticProps and getStaticPaths to Game page

--- src/templates/Game/test.tsx
+++ src/templates/Game/test.tsx
@@ -1,11 +1,16 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+const props = {
+  cover:
+    'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg'
+}
+
 import Game from '.'
 
 describe('<Game />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Game />)
+    renderWithTheme(<Game {...props} />)
 
     expect(screen.getByLabelText(/cover/i)).toBeInTheDocument()
   })

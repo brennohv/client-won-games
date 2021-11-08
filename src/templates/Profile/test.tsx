b0246009f64d0188416b5b7f01c745cbc117a895
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-11-08 15:18:31 +0000

    Add useRouter to activeLink and tests on Profile Template

--- src/templates/Profile/test.tsx
+++ src/templates/Profile/test.tsx
@@ -3,18 +3,37 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Profile from '.'
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({ asPath: '/profile/me' }))
+}))
+
+jest.mock('templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>
+  }
+}))
+
+jest.mock('components/Heading', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Heading">{children}</div>
+  }
+}))
+
+jest.mock('components/ProfileMenu', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock ProfileMenu" />
+  }
+}))
+
 describe('<Profile />', () => {
   it('should render the heading', () => {
-    renderWithTheme(
-      <Profile>
-        <h1>teste</h1>
-      </Profile>
-    )
+    renderWithTheme(<Profile>Lorem Ipsum</Profile>)
 
-    expect(
-      screen.getByRole('heading', { name: /My account/i })
-    ).toBeInTheDocument()
-
-    expect(screen.getByRole('heading', { name: /teste/i })).toBeInTheDocument()
+    expect(screen.getByText('My profile')).toBeInTheDocument()
+    expect(screen.getByText('Lorem Ipsum')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock ProfileMenu')).toBeInTheDocument()
   })
 })

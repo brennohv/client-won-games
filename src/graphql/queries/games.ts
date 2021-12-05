4e787f2a7400c1aafab85aa2899659ccda5631e9
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-12-05 00:02:52 +0000

    Add where and sort to QueryGames

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -3,8 +3,8 @@ import { GameFragment } from './../fragments/game'
 import { gql, QueryHookOptions, useQuery } from '@apollo/client'
 
 export const QUERY_GAMES = gql`
-  query QueryGames($limit: Int!, $start: Int) {
-    games(limit: $limit, start: $start) {
+  query QueryGames($limit: Int!, $start: Int, $where: JSON, $sort: String) {
+    games(limit: $limit, start: $start, where: $where, sort: $sort) {
       ...GameFragment
     }
   }

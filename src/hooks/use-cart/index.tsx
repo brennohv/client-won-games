942dec2929c79e49832d2b45ccd7c8c1c9c3e9e4
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-12-27 23:13:10 +0000

    Add methods to add/remove/clear and check isInCart to useCart

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -1,7 +1,7 @@
 import { useQueryGames } from 'graphql/queries/games'
 import { useContext, createContext, useState, useEffect } from 'react'
 import formatPrice from 'utils/format-price'
-import { getStorageItem } from 'utils/localStorage'
+import { getStorageItem, setStorageItem } from 'utils/localStorage'
 import { cartMapper } from 'utils/mappers'
 
 const CART_KEY = 'cartItems'
@@ -17,12 +17,22 @@ export type CartContextData = {
   items: CartItem[]
   quantity: number
   total: string
+  isInCart: (id: string) => boolean
+  addToCart: (id: string) => void
+  removeFromCart: (id: string) => void
+  clearCart: () => void
+  loading: boolean
 }
 
 export const CartContextDefaultValues = {
   items: [],
   quantity: 0,
-  total: '€0.00'
+  total: '€0.00',
+  isInCart: () => false,
+  addToCart: () => null,
+  removeFromCart: () => null,
+  clearCart: () => null,
+  loading: false
 }
 
 export const CartContext = createContext<CartContextData>(
@@ -48,7 +58,7 @@ const CartProvider = ({ children }: CartProviderProps) => {
   // e passar o valor para o cartItems
   // Que é retornado para o provider
 
-  const { data } = useQueryGames({
+  const { data, loading } = useQueryGames({
     skip: !cartItems.length, //skip faz com que se nao tiver cartItems eu nao rodo a querie
     // Caso eu nao encontre nenhum id ia pegar todos os jogos
     // por isso temos que usar o skip com uma validação se existe algo no storage
@@ -67,12 +77,38 @@ const CartProvider = ({ children }: CartProviderProps) => {
     return acc + game.price
   }, 0)
 
+  const isInCart = (id: string) => (id ? cartItems.includes(id) : false)
+
+  const saveCart = (carItems: string[]) => {
+    setCartItems(carItems)
+    setStorageItem(CART_KEY, carItems)
+  }
+
+  const addToCart = (id: string) => {
+    const newCartItems = [...cartItems, id]
+    saveCart(newCartItems)
+  }
+
+  const removeFromCart = (id: string) => {
+    const newCartItems = cartItems.filter((itemId: string) => itemId !== id)
+    saveCart(newCartItems)
+  }
+
+  const clearCart = () => {
+    saveCart([])
+  }
+
   return (
     <CartContext.Provider
       value={{
         items: cartMapper(data?.games),
         quantity: cartItems.length,
-        total: formatPrice(total || 0)
+        total: formatPrice(total || 0),
+        isInCart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        loading
       }}
     >
       {children}

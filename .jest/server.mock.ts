f5e8b5e989a88944a8afbee9a56e6e48029e693e
Brenno Henrique <brennovicentini@gmail.com>
2022-01-21 14:11:14 +0000

    Configure MSW

---
+++ .jest/server.mock.ts
@@ -0,0 +1,9 @@
+// src/setupTests.js
+import { server } from '../src/utils/mockServer/server'
+// ficar escutando todas as chamadas nos testes
+beforeAll(() => server.listen())
+// reseta todos os handlers para caso eles sejam chamados
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers())
+// fecha o server e limpa os testes
+afterAll(() => server.close())

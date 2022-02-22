08d037aeeec2db16881db2cae8a22ca16ff53a21
Brenno Henrique <brennovicentini@gmail.com>
2022-02-22 14:36:28 +0000

    Add Cypress Testing Library

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,8 +1,17 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should go to go Google', () => {
+  it.skip('should go to go Google', () => {
     cy.google()
-
   })
+
+  it('should visit url, then change dark or ligth theme', () => {
+    cy.visit('https://willianjusten.com.br')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.light').should('exist')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.dark').should('exist')
+  });
 })

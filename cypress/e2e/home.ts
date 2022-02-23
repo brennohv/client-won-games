fe81881fd3aaa9932b1f219fd012a1ebfd9a09b2
Brenno Henrique <brennovicentini@gmail.com>
2022-02-23 17:22:59 +0000

    Start getting banner with get, findByRole and within

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -3,5 +3,13 @@
 describe('Home', () => {
   it('should render home sections', () => {
     cy.visit('/')
+
+    //within é uma callback que cria um escopo dentro do elemento anterior */
+    cy.get('.slick-slider').within(() => {
+      // aqui procuro dentro do .slick-slider se há algum <a> com buy now
+      cy.findByRole('link', { name: /buy now/i})
+      // procurando headings com cyperpunk dentro do .slick-slider
+      cy.findAllByRole('heading', { name: /cyberpunk/i})
+    })
   });
 });

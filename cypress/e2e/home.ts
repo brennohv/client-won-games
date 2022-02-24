94a8f392b8bb318b8178f7401be554dd8450c746
Brenno Henrique <brennovicentini@gmail.com>
2022-02-24 11:23:18 +0000

    Using click and wait to animation on banner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,12 +4,23 @@ describe('Home', () => {
   it('should render home sections', () => {
     cy.visit('/')
 
-    //within é uma callback que cria um escopo dentro do elemento anterior */
+    // within é uma callback que cria um escopo dentro do elemento anterior */
     cy.get('.slick-slider').within(() => {
       // aqui procuro dentro do .slick-slider se há algum <a> com buy now
       cy.findByRole('link', { name: /buy now/i})
       // procurando headings com cyperpunk dentro do .slick-slider
       cy.findAllByRole('heading', { name: /cyberpunk/i})
+      // procurando o button de trocar o banner e clicando nele
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      // esperando o tempo que leva a mudança do banner em milisegundos
+      cy.wait(500)
+      cy.findByRole('link', { name: /buy now/i})
+      cy.findAllByRole('heading', { name: /the witcher/i})
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+
+      cy.findByRole('link', { name: /buy now/i})
+      cy.findAllByRole('heading', { name: /tomb raider/i})
     })
   });
 });

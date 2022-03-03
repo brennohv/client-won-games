6cc7bb268edf5795e4586cd8c81de190c5d725b6
brennohv <brennovicentini@gmail.com>
2022-03-03 12:25:10 +0000

    Search by gameCards and click show more to Games Page

--- cypress/e2e/explorer.ts
+++ cypress/e2e/explorer.ts
@@ -2,19 +2,29 @@
 import { priceFields, categoriesField, platformsFields, sortFields } from '../../src/utils/filter/fields'
 
 describe('Games Page', () => {
-  it('should render fields and headings in games page', () => {
+  before(() => {
     cy.visit('/games')
+  })
 
+  it('should render fields and headings in games page', () => {
     // Headings
     cy.findByRole('heading', {name: /Sort by price/i}).should('exist')
     cy.findByRole('heading', {name: /^Price/i}).should('exist')
     cy.findByRole('heading', {name: /Platforms/i}).should('exist')
     cy.findByRole('heading', {name: /Genres/i}).should('exist')
 
-    //Fields
+    // Fields
     cy.getFields(priceFields)
     cy.getFields(categoriesField)
     cy.getFields(platformsFields)
     cy.getFields(sortFields)
+  });
+
+  it('should render fifteen games and show more games when show more is clicked', () => {
+    cy.getByDataCy('games').should('have.length', 15)
+    cy.findByRole('button', {name: /show more/i})
+      .should('exist')
+      .click()
+    cy.getByDataCy('games').should('have.length', 30)
   });
 });
